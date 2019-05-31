fd7670ffa8bc461554b004b4b11ee08c7cbb6b71
Torku7 <49087696+Torku7@users.noreply.github.com>
2019-05-31 14:19:07 +0900

    현 진행상황입니다

---
+++ map.java
@@ -0,0 +1,45 @@
+package mafia;
+
+import java.util.Scanner;
+
+public class map {
+
+	do{
+		
+		Scanner scan = new Scanner(System.in);
+	if(i==night) {
+		System.out.println("죽이고 싶은 사람의 번호를 선택 해주세요.");
+		int input = scan.nextInt();
+		do {    
+		  switch(1) {
+		     case 1:
+		    	//낮에 1번 이죽었단 메시지 로 보내기 
+		     break;
+		     
+		     case 2:
+			    	//낮에 2번 이죽었단 메시지 로 보내기 
+			     break;
+			     
+		     case 3:
+			    	//낮에 3번 이죽었단 메시지 로 보내기 
+			 break;
+		}
+	  }while(input==n);
+	
+	}
+	
+  }while(n==player); 
+
+	do {
+		do {
+			if(i==night){
+			java.util.Random rand = new java.util.Random();
+			int Randnum;
+			Randnum =rand.nextInt(4);
+			  if(Randnum==) {
+				  
+			  }
+		  }
+		}while(==n)
+	}while(n==com);
+}
